60d7912d51f558e231aac7462746eabdfd2b1902
Andrés García <playsharp59@gmail.com>
2022-01-21 15:50:40 +0100

    test3

--- js/index.js
+++ js/index.js
@@ -2,5 +2,32 @@ const iceBlast = {
     appName: 'Ice Blast',
     author: 'Guillermo Ávila & Andrés García',
     version: 'Beta 0.0.1',
-    gameSize: { w: 500, h: 700 }
+    gameSize: { w: undefined, h: undefined },
+    license: undefined,
+    ctx: undefined,
+
+    init() {
+        this.setContext();
+        this.setSize();
+        this.fillDoc();
+        console.log(this.gameSize);
+    },
+
+    setContext() {
+        this.ctx = document.querySelector('#canvas').getContext('2d');
+    },
+    setSize() {
+        this.gameSize = {
+            w: 550,
+            h: window.innerHeight - 20
+        }
+        document.querySelector('#canvas').setAttribute('width', this.gameSize.w)
+        document.querySelector('#canvas').setAttribute('height', this.gameSize.h)
+    },
+    fillDoc() {
+        this.ctx.fillStyle = 'red';
+        this.ctx.fillRect(0, 0, this.gameSize.w, this.gameSize.h)
+    }
 }
+
+iceBlast.init();
